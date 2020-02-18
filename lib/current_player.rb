d44b921fa88f6bcc169c1dd036d03cdf9008dbe0
Jean-Victor Brun <jeanvbrun@icloud.com>
2020-02-18 23:28:14 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -9,11 +9,13 @@ def turn_count(value)
 end 
 
 
-def current_player(board)
-  count = turn_count(board)
-  if count == 0 || count.even?
-    return "X"
-  else
-    return "O"
-  end
-end 
+urrent_player(board) = count == 0 || count.even? ? "X" : "O"
+
+#def current_player(board)
+#  count = turn_count(board)
+#  if count == 0 || count.even?
+#    return "X"
+#  else
+#    return "O"
+#  end
+#end 
